fb7c749968ba63b3ebbcf55832e9bed5bb2c2e1b
arrazolapp <arrazolapp@gmail.com>
2026-07-21 17:32:13 -0600

    Update MainActivity.java

--- android/app/src/main/java/com/arrazolapp/zocial/MainActivity.java
+++ android/app/src/main/java/com/arrazolapp/zocial/MainActivity.java
@@ -1,5 +1,13 @@
 package com.arrazolapp.zocial;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.gokadzev.capacitormusiccontrols.CapacitorMusicControls;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(CapacitorMusicControls.class);
+        super.onCreate(savedInstanceState);
+    }
+}
